65729f65b402eaddce293e47df219cee95167615
Ankit Panghal <ankitpanghal.work@gmail.com>
2025-01-29 16:48:26 +0000

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -83,7 +83,6 @@ authRouter.get('/logout',(req,res) => {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
-    expires : new Date(0),
    });
     res.status(200).json({
         message : 'User logged out successfully'
